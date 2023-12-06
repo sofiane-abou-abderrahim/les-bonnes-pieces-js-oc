951f09cc95b3bebb41b30adff7141be3dc785341
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-06 15:38:20 +0100

    created elements + updated properties name in json file

--- pieces.js
+++ pieces.js
@@ -1,3 +1,13 @@
 // Retrieve parts from the JSON file
 const response = await fetch('pieces-autos.json');
 const pieces = await response.json();
+
+const article = pieces[0];
+const imageElement = document.createElement('img');
+imageElement.src = article.image;
+const nameElement = document.createElement('h2');
+nameElement.innerText = article.name;
+const priceElement = document.createElement('p');
+priceElement.innerText = `Prix: ${article.price} €`;
+const categoryElement = document.createElement('p');
+categoryElement.innerText = article.category;
