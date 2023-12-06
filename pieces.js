89382e3ba5c49f506abfb079c3b058caf9c4fbe5
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-06 15:44:41 +0100

    attached the element to the rest of the document + updated classes names in index.html

--- pieces.js
+++ pieces.js
@@ -2,6 +2,7 @@
 const response = await fetch('pieces-autos.json');
 const pieces = await response.json();
 
+// creates elements
 const article = pieces[0];
 const imageElement = document.createElement('img');
 imageElement.src = article.image;
@@ -11,3 +12,10 @@ const priceElement = document.createElement('p');
 priceElement.innerText = `Prix: ${article.price} €`;
 const categoryElement = document.createElement('p');
 categoryElement.innerText = article.category;
+
+// attaches the element to the rest of the document
+const sectionSheets = document.querySelector('.sheets');
+sectionSheets.appendChild(imageElement);
+sectionSheets.appendChild(nameElement);
+sectionSheets.appendChild(priceElement);
+sectionSheets.appendChild(categoryElement);
