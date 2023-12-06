3b53a2a539194eb14fa48d74ffbc9826b49ea995
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-07 00:02:53 +0100

    declared new generatePieces function

--- pieces.js
+++ pieces.js
@@ -1,58 +1,53 @@
 // Retrieve parts from the JSON file
 const pieces = await fetch('pieces-autos.json').then(pieces => pieces.json());
 
-for (let i = 0; i < pieces.length; i++) {
-  const article = pieces[i];
+// Function that generates the entire web page
+function generatePieces(pieces) {
+  for (let i = 0; i < pieces.length; i++) {
+    const article = pieces[i];
 
-  // Retrieve the DOM element that will host the cards
-  const sectionSheets = document.querySelector('.sheets');
+    // Retrieve the DOM element that will host the cards
+    const sectionSheets = document.querySelector('.sheets');
 
-  // Create a tag dedicated to an automotive piece
-  const pieceElement = document.createElement('article');
+    // Create a tag dedicated to an automotive piece
+    const pieceElement = document.createElement('article');
 
-  // Create the img element
-  const imageElement = document.createElement('img');
-  // Access index i of the pieces list to configure the image source
-  imageElement.src = article.image;
+    // Create the img element
+    const imageElement = document.createElement('img');
+    // Access index i of the pieces list to configure the image source
+    imageElement.src = article.image;
 
-  const nameElement = document.createElement('h2');
-  nameElement.innerText = article.name;
+    const nameElement = document.createElement('h2');
+    nameElement.innerText = article.name;
 
-  const priceElement = document.createElement('p');
-  priceElement.innerText = `Prix: ${article.price} € (${
-    article.price < 35 ? '€' : '€€€'
-  })`;
+    const priceElement = document.createElement('p');
+    priceElement.innerText = `Prix: ${article.price} € (${
+      article.price < 35 ? '€' : '€€€'
+    })`;
 
-  const categoryElement = document.createElement('p');
-  categoryElement.innerText = article.category ?? '(aucune catégorie)';
+    const categoryElement = document.createElement('p');
+    categoryElement.innerText = article.category ?? '(aucune catégorie)';
 
-  const descriptionElement = document.createElement('p');
-  descriptionElement.innerText =
-    article.description ?? 'Pas de description pour le moment.';
+    const descriptionElement = document.createElement('p');
+    descriptionElement.innerText =
+      article.description ?? 'Pas de description pour le moment.';
 
-  const stockElement = document.createElement('p');
-  stockElement.innerText = article.availability
-    ? 'En stock'
-    : 'Rupture de stock';
+    const stockElement = document.createElement('p');
+    stockElement.innerText = article.availability
+      ? 'En stock'
+      : 'Rupture de stock';
 
-  // Attach the article tag to the Sheets section
-  sectionSheets.appendChild(pieceElement);
-  // Attach the image to pieceElement (the article tag)
-  pieceElement.appendChild(imageElement);
-  pieceElement.appendChild(nameElement);
-  pieceElement.appendChild(priceElement);
-  pieceElement.appendChild(categoryElement);
-  pieceElement.appendChild(descriptionElement);
-  pieceElement.appendChild(stockElement);
+    // Attach the article tag to the Sheets section
+    sectionSheets.appendChild(pieceElement);
+    // Attach the image to pieceElement (the article tag)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nameElement);
+    pieceElement.appendChild(priceElement);
+    pieceElement.appendChild(categoryElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+  }
 }
-
-// const sortButton = document.querySelector('.btn-sort');
-// sortButton.addEventListener('click', function () {
-//   pieces.sort(function (a, b) {
-//     return a.price - b.price;
-//   });
-//   console.log(pieces);
-// });
 
 // sort button management
 const sortButton = document.querySelector('.btn-sort');
