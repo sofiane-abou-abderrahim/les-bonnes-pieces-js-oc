21f454ad1c4a6c8f1de2ad92a7d12b3b5841ae43
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-06 15:47:52 +0100

    corrected comments

--- pieces.js
+++ pieces.js
@@ -2,7 +2,7 @@
 const response = await fetch('pieces-autos.json');
 const pieces = await response.json();
 
-// creates elements
+// create elements
 const article = pieces[0];
 const imageElement = document.createElement('img');
 imageElement.src = article.image;
@@ -13,7 +13,7 @@ priceElement.innerText = `Prix: ${article.price} €`;
 const categoryElement = document.createElement('p');
 categoryElement.innerText = article.category;
 
-// attaches the element to the rest of the document
+// attache the element to the rest of the document
 const sectionSheets = document.querySelector('.sheets');
 sectionSheets.appendChild(imageElement);
 sectionSheets.appendChild(nameElement);
